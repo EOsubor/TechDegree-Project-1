d24d305833538a629c1f3a50d112d623d3ca42af
Ekene Osubor <ekeneosubor@gmail.com>
2018-12-24 18:01:54 +0000

    Refactoring

--- guessing_game.py
+++ guessing_game.py
@@ -1,23 +1,24 @@
 import random
 import time
+import sys
 
 
 def roll_out():
-    time.sleep(.6)
+    time.sleep(.55)
+    print("-----------------------------------------")
+    time.sleep(.55)
     
 def start_game():
     correct_guess = random.randint(1, 21)
     number_range = list(range(1, 21))
     number_of_tries = 0
+    high_score = 0
     
     roll_out()
-    print("-------------------------------------")
+    print("  Welcome to the Numbers Guessing Game.")
     roll_out()
-    print("Welcome to the Numbers Guessing Game.")
-    roll_out()
-    print("-------------------------------------")
     
-    while True:        
+    while True:
         number_of_tries +=1
         try:
             roll_out()
@@ -35,24 +36,21 @@ def start_game():
         elif player_guess == correct_guess:
             print(f"\nThat's correct! It took you {number_of_tries} tries!")
             replay = input("\nWould you like to play again? (Y/N)\n")
-            if replay.upper() == 'Y':                
-                roll_out()
-                print("-----------------------------------------")
+            if replay.upper() == 'Y':
+                correct_guess = random.randint(1, 21)
+                high_score = number_of_tries
+                if number_of_tries < high_score:
+                    high_score = number_of_tries
                 roll_out()
                 print(f"\tThe current highscore is {high_score}")
                 roll_out()
-                print("-----------------------------------------")
-                roll_out()
                 number_of_tries = 0
                 continue
-            else:
-                roll_out()
-                print("-----------------------------------------")
+            elif replay.upper() == 'N':
                 roll_out()
                 print("\t\tGAME OVER!!\n\t    Thanks for playing!")
                 roll_out()
-                print("-----------------------------------------")
-                break         
+                sys.exit()         
             
     
 if __name__ == '__main__':

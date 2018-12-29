f673a6abf0161908bf7b7fee61f35bc869667011
Ekene Osubor <ekeneosubor@gmail.com>
2018-12-29 04:32:21 +0000

    Refactoring & Debugging
    Added nested while loop for replay input. Fixed number_range error, where error wasn't bubbled up to caller properly.

--- guessing_game.py
+++ guessing_game.py
@@ -1,58 +1,75 @@
 import random
 import time
 import sys
+import os
+
+
+def get_lowest_number(high_score, number_of_tries):
+    if high_score == 0:
+        high_score = number_of_tries
+    elif number_of_tries < high_score:
+        high_score = number_of_tries
+    return high_score
+
+
+def clear_screen():
+    time.sleep(1.5)
+    os.system("cls" if os.name == "nt" else "clear")
+
 
 def roll_out():
     time.sleep(.55)
     print("-----------------------------------------")
     time.sleep(.55)
-    
-    
+
+
 def start_game():
     correct_guess = random.randint(1, 21)
     number_range = list(range(1, 21))
     number_of_tries = 0
     high_score = 0
-    
+
+
     roll_out()
     print("  Welcome to the Numbers Guessing Game.")
     roll_out()
-    
+
     while True:
-        number_of_tries +=1
         try:
             roll_out()
             player_guess = int(input("\nPick a number between 1 and 20:\n"))
-            if player_guess not in number_range:
-                raise ValueError("Number must be between 1 and 20")
-        except ValueError as err:
-            print("Oh no, Invalid input. Please try again")
-            print(f"{err}")
+        except ValueError:
+            print("\nOh no, Invalid input. Please try again")
+            print("Number must be between 1 and 20")
             continue
         if player_guess > correct_guess:
+            number_of_tries +=1
             print("\nTry again! The number you picked is higher")
         elif player_guess < correct_guess:
-            print("Try again! The number you picked is lower")    
+            number_of_tries +=1
+            print("Try again! The number you picked is lower")
         elif player_guess == correct_guess:
+            number_of_tries +=1
             print(f"\nThat's correct! It took you {number_of_tries} tries!")
-            replay = input("\nWould you like to play again? (Y/N)\n")
-            if replay.upper() == 'Y':
-                correct_guess = random.randint(1, 21)
-                high_score = number_of_tries
-                if number_of_tries < high_score:
-                    high_score = number_of_tries
-                roll_out()
-                print(f"\tThe current highscore is {high_score}")
-                roll_out()
-                number_of_tries = 0
-                continue
-            elif replay.upper() == 'N':
-                roll_out()
-                print("\t\tGAME OVER!!\n\t    Thanks for playing!")
-                roll_out()
-                sys.exit()         
-            
-    
+            while True:
+                replay = input("\nWould you like to play again? (Y/N)\n")
+                if replay.upper() == 'Y':
+                    correct_guess = random.randint(1, 21)
+                    high_score = get_lowest_number(high_score, number_of_tries)
+                    roll_out()
+                    print(f"\tThe current highscore is {high_score}")
+                    roll_out()
+                    number_of_tries = 0
+                    clear_screen()
+                    break
+                elif replay.upper() == 'N',:
+                    roll_out()
+                    print("\t\tGAME OVER!!\n\t    Thanks for playing!")
+                    roll_out()
+                    clear_screen()
+                    sys.exit()
+                #replay = input("\nWould you like to play again? (Y/N)\n")
+
+
 if __name__ == '__main__':
-    # Kick off the program by calling the start_game function.
     start_game()
